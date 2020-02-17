4d1972320f182c489346d70e9dad13bf7e0aeba0
Vincent Semrau <vincent.semrau@calimoto.com>
2020-02-17 14:34:35 +0100

    renamed a little bit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const utils = require("./utils");
 module.exports = function () {
     this.addDependency(this.resourcePath);
     const file = require("fs").readFileSync(this.resourcePath);
-    const bufferArrayString = new Uint8Array(file).toString();
+    const bufferString = new Uint8Array(file).toString();
     return `module.exports = (() => {
-        return ${utils.convertBufferArrayStringToInstance}.bind({}, "${bufferArrayString}");
+        return ${utils.convertBufferStringToInstance}.bind({}, "${bufferString}");
     })()`;
 }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-module.exports.convertBufferArrayStringToInstance = async function (bufferArrayString) {
+module.exports.convertBufferStringToInstance = async function (bufferArrayString) {
     const buffer = new Uint8Array(bufferArrayString.split(","));
     const module = await WebAssembly.compile(buffer);
     return new WebAssembly.Instance(module);
